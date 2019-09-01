cb962a31e5302227deadce32811a9b2794e2000f
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-09-01 01:46:51 -0600

    commit decorators

--- ejemplo.js
+++ ejemplo.js
@@ -1,7 +1,44 @@
-var test = /** @class */ (function () {
-    function test(b, c) {
-        this.b = b;
-        this.c = c;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function log(constructor) {
+    console.log('Registered	Class: ' + constructor['name'] + 'at ' + Date.now());
+}
+function logm(target, propertyKey, descriptor) {
+    console.log('registered method: ' + propertyKey.toString() + ' at ' + Date.now());
+}
+function logparam(target, propertyKey, parameterINdex) {
+    console.log('Registered parameter: ' + propertyKey.toString() + ' - ' + parameterINdex + ' at ' + Date.now());
+}
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+        this.name = 'name';
+        console.log('constructor');
     }
-    return test;
+    MyClass.prototype.myMethod = function () {
+        console.log('method');
+    };
+    MyClass.prototype.myMethod2 = function (param1, param2) {
+        console.log('mehotd2');
+    };
+    __decorate([
+        logm
+    ], MyClass.prototype, "myMethod");
+    __decorate([
+        logm,
+        __param(1, logparam)
+    ], MyClass.prototype, "myMethod2");
+    MyClass = __decorate([
+        log
+    ], MyClass);
+    return MyClass;
 }());
+var myClass = new MyClass();
+myClass.myMethod();
+myClass.myMethod2(1, false);
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,16 +1,33 @@
-interface a {
-    b : number;
+function log(constructor: Function): void{
+    console.log('Registered	Class: ' + constructor['name'] +'at ' + Date.now()); 
 }
 
-interface b extends a{
-    c: string;
+function logm<T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): void {
+    console.log('registered method: ' + propertyKey.toString() + ' at ' + Date.now());
 }
 
-class test implements b{
-    b : number;
-    c: string;
-    constructor( b: number, c: string){
-        this.b = b;
-        this.c = c;
+function logparam (target: Object, propertyKey: string | symbol, parameterINdex: number): void{
+    console.log('Registered parameter: ' + propertyKey.toString() + ' - ' + parameterINdex + ' at ' + Date.now());
+}
+
+@log
+class MyClass{
+    public name: string = 'name';
+    constructor() {
+        console.log('constructor');
+    }
+
+    @logm
+    public myMethod(){
+        console.log('method')
+    }
+
+    @logm
+    public myMethod2(param1: number, @logparam param2: boolean){
+        console.log('mehotd2')
     }
 }
+
+var myClass = new MyClass();
+myClass.myMethod();
+myClass.myMethod2(1, false);
