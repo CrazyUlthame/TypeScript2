54cc18712edd0e28390fe2b6451f3c46baf4ba30
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-08-31 23:09:53 -0600

    commit desestructuracion3

--- ejemplo.js
+++ ejemplo.js
@@ -1,4 +1,10 @@
-var array = [1, 2, 3];
-console.log(array[0]);
-var x = array[0], y = array[1], z = array[2];
-console.log(x);
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var array = [1, 2, 3, 4];
+var x = array[0], y = array[1], rest = array.slice(2);
+console.log.apply(console, __spreadArrays([x, y], rest));
