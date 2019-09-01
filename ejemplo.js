adf3550d9778ecbdeff22ff4ea085432580ae01d
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-08-31 23:33:05 -0600

    commit promise7?

--- ejemplo.js
+++ ejemplo.js
@@ -1,10 +1,11 @@
-Promise.reject(new Error('something bad happened'))
+function iReturnPromiseAfter1Second() {
+    return new Promise(function (resolve) {
+        setTimeout(function () { return resolve("hello world!"); }, 1000);
+    });
+}
+Promise.resolve(123)
     .then(function (res) {
-    console.log(res);
-    return 456;
-})["catch"](function (err) {
-    console.log(err.message);
-    return Promise.resolve(123);
+    return iReturnPromiseAfter1Second();
 })
     .then(function (res) {
     console.log(res);
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,12 +1,13 @@
-Promise.reject(new Error('something bad happened'))
+function iReturnPromiseAfter1Second():Promise<string> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve ("hello world!"), 1000);
+    });
+}
+
+Promise.resolve(123)
+    .then((res) => {
+        return iReturnPromiseAfter1Second();
+    })
     .then((res) => {
         console.log(res);
-        return 456;
-    })
-    .catch((err) => {
-        console.log(err.message);
-        return Promise.resolve(123);
-    })
-    .then((res) => {
-        console.log(res);    
-    })  
+    });
