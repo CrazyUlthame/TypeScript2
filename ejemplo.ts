dd0cd3f7ea201f7ccaaf5db833cbf1d1398b1329
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-09-01 00:56:37 -0600

    commit sobrecarga

--- ejemplo.ts
+++ ejemplo.ts
@@ -1,6 +1,7 @@
-class Base {
-    log() { console.log("hello world");}
-}
-class Child extends Base{
-    log(){ super.log()};
+function a(x: number);
+function a(x: string);
+function a(x: boolean);
+function a(x: Array<number>);
+function a(x){
+    
 }
