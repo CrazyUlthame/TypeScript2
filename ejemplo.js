67241d2d5bf2a18977cbf177d5e34074e318ace4
neto soto <Ernesto.Soto.1998@hotmail.com>
2019-09-01 01:29:40 -0600

    commit interfaces

--- ejemplo.js
+++ ejemplo.js
@@ -1,41 +1,7 @@
-var Disponible = /** @class */ (function () {
-    function Disponible() {
+var test = /** @class */ (function () {
+    function test(b, c) {
+        this.b = b;
+        this.c = c;
     }
-    Disponible.prototype.dispose = function () {
-        this.isDisposed = true;
-    };
-    return Disponible;
+    return test;
 }());
-var Activatable = /** @class */ (function () {
-    function Activatable() {
-    }
-    Activatable.prototype.activate = function () {
-        this.isActive = true;
-    };
-    Activatable.prototype.desctivate = function () {
-        this.isActive = false;
-    };
-    return Activatable;
-}());
-var SmartObject = /** @class */ (function () {
-    function SmartObject() {
-        var _this = this;
-        this.isDisposed = false;
-        this.isActive = true;
-        setInterval(function () { return console.log(_this.isActive + " : " + _this.isDisposed); }, 500);
-    }
-    SmartObject.prototype.interact = function () {
-        this.activate();
-    };
-    return SmartObject;
-}());
-applyMixins(SmartObject, [Disponible, Activatable]);
-var smartObj = new SmartObject();
-setTimeout(function () { return smartObj.interact(); }, 1000);
-function applyMixins(derivedCtor, baseCtors) {
-    baseCtors.forEach(function (baseCtors) {
-        Object.getOwnPropertyNames(baseCtors.prototype).forEach(function (name) {
-            derivedCtor.prototype[name] = baseCtors.prototype[name];
-        });
-    });
-}
--- ejemplo.ts
+++ ejemplo.ts
@@ -1,44 +1,16 @@
-class Disponible {
-    isDisposed: boolean;
-    dispose(){
-        this.isDisposed = true;
-    }
+interface a {
+    b : number;
 }
 
-class Activatable {
-    isActive: boolean;
-    activate(){
-        this.isActive = true;
-    }
-    desctivate(){
-        this.isActive = false;
-    }
+interface b extends a{
+    c: string;
 }
 
-class SmartObject implements Disponible, Activatable {
-    constructor(){
-        setInterval(() => console.log(this.isActive + " : " + this.isDisposed),500);
+class test implements b{
+    b : number;
+    c: string;
+    constructor( b: number, c: string){
+        this.b = b;
+        this.c = c;
     }
-    interact(){
-        this.activate()
-    }
-
-    isDisposed: boolean = false;
-    dispose: () => void;
-
-    isActive: boolean = true;
-    activate: () => void;
-    desctivate: () => void;
-}
-
-applyMixins(SmartObject, [Disponible,Activatable]);
-
-let smartObj = new SmartObject();
-setTimeout(() => smartObj.interact(), 1000);
-function applyMixins(derivedCtor: any, baseCtors: any[]){
-    baseCtors.forEach(baseCtors => {
-        Object.getOwnPropertyNames(baseCtors.prototype).forEach(name => {
-            derivedCtor.prototype[name] = baseCtors.prototype[name];
-        });
-    });
 }
